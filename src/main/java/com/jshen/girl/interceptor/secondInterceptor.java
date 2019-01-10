b3685942ad74d94c9caf301ff66101d5bf153fc6
Yunxiso <634502342@qq.com>
2019-01-11 00:23:39 +0800

    Merge branch 'master' of github.com:YUNXISO/girl
    # 进度:
    #	拦截器的使用

---
+++ src/main/java/com/jshen/girl/interceptor/secondInterceptor.java
@@ -0,0 +1,77 @@
+package com.jshen.girl.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class secondInterceptor implements HandlerInterceptor {
+    /**
+     * 进入controller之前
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request
+            , HttpServletResponse response
+            , Object handler) throws Exception {
+
+        System.out.println("secondInterceptor------->preHandle");
+
+//        String token = request.getParameter("access_token");
+//        /*
+//        *业务操作
+//        */
+//        response.getWriter().print("错误");
+//        response.getWriter().flush();
+//        response.getWriter().close();
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    /**
+     * 调用完controller后，视图渲染前
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request
+            , HttpServletResponse response
+            , Object handler
+            , ModelAndView modelAndView) throws Exception {
+
+        System.out.println("secondInterceptor------->postHandle");
+
+        HandlerInterceptor.super.postHandle(request,response,handler,modelAndView);
+
+    }
+
+    /**
+     * 整个完成之后，通常用于资源清理
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request
+            , HttpServletResponse response
+            , Object handler
+            , Exception ex) throws Exception {
+
+        System.out.println("secondInterceptor------->afterCompletion");
+
+        HandlerInterceptor.super.afterCompletion(request,response,handler,ex);
+
+    }
+}
