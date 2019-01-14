6f5bbfbbf2d8276d6b62955ddbf04ae20760f30e
Yunxiso <634502342@qq.com>
2019-01-15 02:39:20 +0800

    规范类名

--- src/main/java/com/jshen/girl/interceptor/CustomWebMvcConfigurer.java
+++ src/main/java/com/jshen/girl/interceptor/CustomWebMvcConfigurer.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class customWebMvcController implements WebMvcConfigurer {
+public class CustomWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/hello");
